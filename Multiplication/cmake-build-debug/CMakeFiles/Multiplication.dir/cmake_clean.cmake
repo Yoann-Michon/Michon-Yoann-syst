544956603f8bf32e6d04d4f15c0a4a422ba8a54a
Yoann Michon <62294558+Yoann-Michon@users.noreply.github.com>
2020-03-18 16:51:07 +0100

    Add files via upload

---
+++ Multiplication/cmake-build-debug/CMakeFiles/Multiplication.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Multiplication.dir/Multiplication.c.obj"
+  "Multiplication.exe"
+  "Multiplication.exe.manifest"
+  "Multiplication.pdb"
+  "libMultiplication.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Multiplication.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
