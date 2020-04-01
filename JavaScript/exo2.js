a9cbac964026791628282e3d852bdf4c0fe1b289
Yoyo <michon.yoann@outlook.com>
2020-04-01 09:44:45 +0200

    exercices

--- JavaScript/exo2.js
+++ JavaScript/exo2.js
@@ -24,3 +24,27 @@ function fibonacci(nbr) {
 console.log(fibonacci(8));
 
 //2.4
+function creerMultiplicateur2(n,x){
+    if (x === null)
+        return (k) => k* n;
+    else
+        return n * x;
+}
+console.log(creerMultiplicateur2(5,2));
+
+//2.5
+function power2(n, x){
+    let res;
+    if (x === null){
+        return(power2(n-1)*n);
+    }
+    if (x === 0){return 1;}
+    res = n*power2(n,x-1);
+    return res;
+}
+console.log("5 Puissance 2 : " + power2(5,2));
+
+//2.6
+function formatter(){
+    
+}
