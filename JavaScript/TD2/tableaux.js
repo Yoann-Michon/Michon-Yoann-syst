d646909ccc95072ae36fa9f57139c8f779a63a63
Yoyo <michon.yoann@outlook.com>
2020-04-03 11:32:27 +0200

    exercices 1

--- JavaScript/TD2/tableaux.js
+++ JavaScript/TD2/tableaux.js
@@ -1,7 +1,12 @@
 'use strict';
 
 function range(a,b){
+    let tab=[];
     let min=Math.min(a,b);
     let max=Math.max(a,b);
-
+    for (let i=min;i<=max;i++){
+        tab.push(i);
+    }return tab;
 }
+console.log("de 1 à 5 : "+range(1,5));
+console.log("de 1 à 5 : "+range(5,1));
